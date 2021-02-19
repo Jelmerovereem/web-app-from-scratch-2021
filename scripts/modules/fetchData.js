db5e0ad8064df8b56a895f7b7baff86ef90de407
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-02-19 09:36:53 +0100

    added fetchdata function for refactoring

--- scripts/modules/fetchData.js
+++ scripts/modules/fetchData.js
@@ -2,7 +2,7 @@ import { apikey } from "../utilities/key.js";
 import { errMsg } from "../utilities/renderUtils.js";
 import showErr from "./showError.js";
 
-export {getWeatherCity, getWeatherCoords}
+export {getWeatherCity, getWeatherCoords, fetchData}
 
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 const units = "metric";
@@ -23,5 +23,10 @@ function getWeatherCity(city) {
 /* fetch weather data from openweather api based on coords */
 function getWeatherCoords(lat, lon) {
 	return fetch(`${baseUrl}?lat=${lat}&lon=${lon}&units=${units}&appid=${apikey}`)
+	.then(response => response.ok ? response.json() : console.log(response.ok))
+}
+
+function fetchData(url) {
+	return fetch(url)
 	.then(response => response.ok ? response.json() : console.log(response.ok))
 }
